d6ec12bc0b18f4bcd79f89f24ccc224b345e128e
Roberto Besser <rmbesser@gmail.com>
2016-05-05 19:50:04 -0300

    First commit

---
+++ isola/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+type position struct {
+	x, y int
+}
+
+func (p position) isNeighbour(x, y int) (is bool) {
+	if !(p.x == x && p.y == y) && math.Abs((float64)(p.x-x)) <= 1 && math.Abs((float64)(p.y-y)) <= 1 {
+		is = true
+	}
+	return is
+}
+
+func main() {
+	player := position{1, 1}
+	fmt.Println(player.isNeighbour(1, 1))
+}
